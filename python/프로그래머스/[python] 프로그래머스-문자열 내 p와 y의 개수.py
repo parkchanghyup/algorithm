8b5cc5d27a2d4acc1ecd6b92f476533b47e2eac7
parkchanghyup <57655448+parkchanghyup@users.noreply.github.com>
2022-07-13 22:58:39 +0900

    프로그래머스

---
+++ python/프로그래머스/[python] 프로그래머스-문자열 내 p와 y의 개수.py
@@ -0,0 +1,15 @@
+def solution(s):
+    
+    
+    s = s.lower()
+    
+    p, y = 0, 0
+    
+    for char in s :
+        if char == 'p':
+            p+=1
+        elif char =='y':
+            y+=1
+        
+
+    return (p==y)
