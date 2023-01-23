4ec563e1db5002d90f592444a1c56e67dc112925
parkchanghyup <57655448+parkchanghyup@users.noreply.github.com>
2023-01-24 00:15:14 +0900

    파이썬

--- python/프로그래머스/[python] 프로그래머스-마법의 엘리베이터.py
+++ python/프로그래머스/[python] 프로그래머스-마법의 엘리베이터.py
@@ -3,39 +3,32 @@ def solution(storey):
     storey_list = []
     for num in storey:
         storey_list.append(int(num))
-        
-        
-    
+
     cnt = len(storey)
     idx = cnt - 1
     answer = 0
-    
+
     while idx > 0:
 
-        if storey_list[idx] == 5 and storey_list[idx-1] == 9:
-            answer += 6
-            storey_list[idx-1] += 1
-        
-        elif storey_list[idx] > 5 :
-            answer = answer + (10 - storey_list[idx] )
+        if storey_list[idx] == 5 and storey_list[idx-1] > 4:
+            answer += 5
             storey_list[idx-1] += 1
 
-        else :
+        elif storey_list[idx] > 5:
+            answer = answer + (10 - storey_list[idx])
+            storey_list[idx-1] += 1
+
+        else:
             answer += storey_list[idx]
-        
+
         idx -= 1
-        
-    
-    if storey_list[0] == 9:
-        return answer + 2
-    
-    elif storey_list[0] ==10:
+
+    if storey_list[0] == 10:
         return answer + 1
-    else :
-        return answer + min(10-storey_list[0],storey_list[0])
-        
-        
-    return answer
+    elif storey_list[0] > 5:
+        return answer + (10 - storey_list[0]) + 1
+    else:
+        return answer + storey_list[0]
 
-solution(95)
 
+solution(75)
